09ee86e1f0903de000c316eb7e881d6459fe4aef
epond <epond@users.noreply.github.com>
2014-12-21 16:54:47 +0000

    Implement rotate

--- 02-starting-out.hs
+++ 02-starting-out.hs
@@ -53,4 +53,4 @@ insertElem x k l = (take k l) ++ [x] ++ (drop k l)
 
 -- Rotate list l n places left.
 -- For example, "rotate 2 [1,2,3,4,5]" gives [3,4,5,1,2]
-rotate n l = undefined
+rotate n l = (drop n l) ++ (take n l)
