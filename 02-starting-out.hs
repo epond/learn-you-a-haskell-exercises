b7e76cfd211a1aa804ace6d8c384c372e3905684
epond <epond@users.noreply.github.com>
2014-12-21 16:50:13 +0000

    Implement slice

--- 02-starting-out.hs
+++ 02-starting-out.hs
@@ -45,7 +45,7 @@ dropK k l = (take k l) ++ (drop (k+1) l)
 
 -- Extract elements between ith and kth element in list l. Including i, but not k
 -- For example, "slice 3 6 [0,0,0,1,2,3,0,0,0]" returns [1,2,3]
-slice i k l = undefined
+slice i k l = drop i (take k l)
 
 -- Insert element x in list l at index k
 -- For example, "insertElem 2 5 [0,0,0,0,0,0]" returns [0,0,0,0,0,2,0]
