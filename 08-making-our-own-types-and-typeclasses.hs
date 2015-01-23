3304b9c14431a8bc4a37fb11dbdc802f38686613
Ed Pond <edward.pond@springer.com>
2015-01-23 14:42:52 +0000

    Implement 08-making-our-own-types-and-typeclasses

--- 08-making-our-own-types-and-typeclasses.hs
+++ 08-making-our-own-types-and-typeclasses.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 {-
  - We are going to create some types for a deck of cards
  - The cards need to have an ordering, based on the standard ranking http://en.wikipedia.org/wiki/Standard_52-card_deck#Rank_and_color
@@ -12,13 +14,16 @@
  - Uncomment the following declarations to complete the implementation, and provide an implementation for instance Show Card
  -}
 
---data Suit = 
---data Digit = 
---data Card = 
+data Suit = Clubs | Spades | Diamonds | Hearts deriving (Read, Show, Eq, Ord, Bounded)
+data Digit = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace deriving (Read, Show, Eq, Ord, Bounded)
+data Card = Card Digit Suit deriving (Eq, Ord)
+
+instance Show Card where
+	show (Card d s) = show d ++ " of " ++ show s
 
 -- We should be able to provide a function which returns the higher ranked card:
 betterCard :: Card -> Card -> Card
-betterCard x y = undefined
+betterCard x y = max x y
 
 -- Here is a new Typeclass, which represents some kind of playing hand in a game.
 -- It returns True for a "winning hand", depending on the rules for the type of class we are playing with
@@ -27,13 +32,17 @@ class Hand a where
 
 -- Implement Hand for Card, where play returns true if the list contains the Ace of Spades
 instance Hand Card where
-    play c = undefined
+    play c = elem (Card Ace Spades) c
 
 -- Create a new Coin type
---data Coin = 
+data Coin = Heads | Tails deriving (Read, Show, Eq, Ord, Bounded)
 
 -- Implement Hand for Coin, where play returns true if there are ten heads in a row in the list
 instance Hand Coin where
-	play c =  undefined
+	play c = isInfixOf (replicate 10 Heads) c
 
 -- Have a play with implementing Hand for some other types, for instance Int and Bool
+
+-- Let's say for Int, a winning hand is three 7s in a row
+instance Hand Int where
+	play c = isInfixOf (replicate 3 7) c
