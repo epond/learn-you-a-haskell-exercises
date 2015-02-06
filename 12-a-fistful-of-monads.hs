140909cdfc0c5126a59a7781abb02be712911c8f
Ed Pond <edward.pond@springer.com>
2015-02-06 15:32:11 +0000

    Implement 12-a-fistful-of-monads

--- 12-a-fistful-of-monads.hs
+++ 12-a-fistful-of-monads.hs
@@ -5,27 +5,35 @@
  -   Success, which takes that parameter and
  -   Fail String, which represents a failure, and a reason for that failure
  -}
---data Validation a 
+data Validation a = Success a | Fail String deriving Show
 
 -- Make the Validation a Monad
-
+instance Monad Validation where
+	return x = Success x
+	Fail x >>= f = Fail x
+	(Success a) >>= f = f a
+	fail msg = Fail msg
 
 {-
  - Create a function, positiveCheck, which takes a number and returns a successful Validation if it's positive, 
  - and a failed Validation with a String message if not.
  -}
---positiveCheck :: (Num a, Ord a) => a -> Validation a
---positiveCheck x = undefined
+positiveCheck :: (Num a, Ord a) => a -> Validation a
+positiveCheck x = if x > 0
+	then Success x
+	else Fail "Number was not positive"
 
 {-
  - Create a function, evenCheck, which returns a successful Validation if it's even,
  - and a failed Validation with a string message if it's odd
  -}
---evenCheck :: (Integral a)  =>  a -> Validation a
---evenCheck x = undefined
+evenCheck :: (Integral a)  =>  a -> Validation a
+evenCheck x = if (mod x 2) == 0
+	then Success x
+	else Fail "Number was not even"
 
 {-
  - Write a function which uses positiveCheck and evenCheck to make sure a number is both positive and even
  -}
---positiveAndEvenCheck :: (Num a, Ord a, Integral a) => a -> Validation a
---positiveAndEvenCheck x = undefined
+positiveAndEvenCheck :: (Num a, Ord a, Integral a) => a -> Validation a
+positiveAndEvenCheck x = (return x) >>= positiveCheck >>= evenCheck
